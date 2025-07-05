d15f13fea1476d91f06510b70c9f9a515ee34740
Ashlok Chaudhary <chaudharyashlok@gmail.com>
2025-07-05 23:25:27 +0530

    Updating Skills

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,7 +15,7 @@ export const skills: Skill[] = [
   { skill_name: 'Redux', Image: '/redux.png', width: 80, height: 80 },
   { skill_name: 'React Query', Image: '/reactquery.png', width: 80, height: 80 },
   { skill_name: 'Tailwind CSS', Image: '/tailwind.png', width: 80, height: 80 },
-  { skill_name: 'Material UI', Image: '/mui.png', width: 80, height: 80 },
+  { skill_name: 'GitLab', Image: '/gitlab.png', width: 80, height: 80 },
   { skill_name: 'Node.js', Image: '/node-js.png', width: 80, height: 80 },
   { skill_name: 'Express.js', Image: '/express.png', width: 80, height: 80 },
   { skill_name: 'Spring Boot', Image: '/spring-boot.png', width: 55, height: 55 },
@@ -29,7 +29,7 @@ export const skills: Skill[] = [
   { skill_name: 'AWS', Image: '/aws.png', width: 70, height: 70 },
   { skill_name: 'Rust', Image: '/rust.png', width: 70, height: 70 },
   { skill_name: 'Go', Image: '/go.png', width: 60, height: 60 },
-  { skill_name: 'Stripe', Image: '/stripe.webp', width: 80, height: 80 },
+  { skill_name: 'CloudFlare', Image: '/cloudflare.png', width: 80, height: 80 },
 ]
 
 export const Socials = [
