7dd9420890fd47d696eb9842349b9dd98065bc8b
Cristian Casis <crcasis@gmail.com>
2025-10-19 12:29:18 +0100

    update skills

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,13 +14,11 @@ export const skills: Skill[] = [
   { skill_name: 'Datadog', Image: '/datadog.webp', width: 80, height: 80 },
   { skill_name: 'Linux', Image: '/linux.webp', width: 80, height: 80 },
   { skill_name: 'Nginx', Image: '/nginx.png', width: 80, height: 80 },
-  { skill_name: 'Jira', Image: '/jira.png', width: 80, height: 80 },
   { skill_name: 'GitLab', Image: '/gitlab.png', width: 80, height: 80 },
   { skill_name: 'Python', Image: '/python.jpeg', width: 80, height: 80 },
   { skill_name: 'GCloud', Image: '/gcloud.png', width: 80, height: 80 },
   { skill_name: 'Azure', Image: '/azure.png', width: 55, height: 55 },
   { skill_name: 'PostgreSQL', Image: '/postgresql.png', width: 70, height: 70 },
-  { skill_name: 'MySQL', Image: '/mysql.png', width: 70, height: 70 },
   { skill_name: 'Ansible', Image: '/ansible.png', width: 70, height: 70 },
   { skill_name: 'Redhat', Image: '/redhat.png', width: 80, height: 80 },
   { skill_name: 'Terraform', Image: '/terraform.png', width: 80, height: 80 },
@@ -31,10 +29,8 @@ export const skills: Skill[] = [
   { skill_name: 'Zabbix', Image: '/zabbix.png', width: 60, height: 60 },
   { skill_name: 'CloudFlare', Image: '/cloudflare.png', width: 80, height: 80 },
   { skill_name: 'Jenkins', Image: '/jenkins.png', width: 80, height: 80 },
-  { skill_name: 'Confluence', Image: '/confluence.png', width: 80, height: 80 },
   { skill_name: 'Kustomize', Image: '/kustomize.png', width: 80, height: 80 },
   { skill_name: 'VertexAI', Image: '/vertex.png', width: 80, height: 80 },
-  { skill_name: 'AzureDevOps', Image: '/azure-devops.png', width: 80, height: 80 },
 ]
 
 export const Socials = [
